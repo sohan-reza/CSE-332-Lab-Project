adc01cf81f932d8f0a477e3e153818e980f0c4a6
Sohan Reza <sohanreja22@gmail.com>
2023-10-13 10:55:30 +0600

    UserHandler class implemented

--- src/server/Server.java
+++ src/server/Server.java
@@ -82,7 +82,7 @@ public class Server {
 
 
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        new Server(5050).run();
     }
 }
--- src/server/UserHandler.java
+++ src/server/UserHandler.java
@@ -1,4 +1,41 @@
 package server;
 
-public class UserHandler {
+import java.util.Scanner;
+
+public class UserHandler implements Runnable {
+    private Server server;
+    private User user;
+
+    public UserHandler(Server server, User user) {
+        this.server = server;
+        this.user = user;
+        this.server.broadcastAllUsers();
+    }
+
+    @Override
+    public void run() {
+        String message;
+
+        Scanner scanner = new Scanner(this.user.getInputStream());
+        while(scanner.hasNextLine()) {
+            message = scanner.nextLine();
+
+            if(message.charAt(0) == '@') {
+                if(message.contains(" ")) {
+                    System.out.println("Private message: " + message);
+                    int firstSpace = message.indexOf(" ");
+                    String receiver = message.substring(1, firstSpace);
+                    String extractMessage = message.substring(firstSpace+1);
+
+                    server.sendMessageToUser(extractMessage, user, receiver);
+                }
+            }else {
+                server.broadcastMessages(message, user);
+            }
+        }
+
+        server.removeUser(user);
+        this.server.broadcastAllUsers();
+        scanner.close();
+    }
 }
