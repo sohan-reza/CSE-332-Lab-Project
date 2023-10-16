b7d9496240518d72109416a447bfb72f855cc468
Sohan Reza <sohanreja22@gmail.com>
2023-10-16 21:47:16 +0600

    WIP: activer user box implemented, now start implementing button, input box

--- src/client/Client.java
+++ src/client/Client.java
@@ -28,6 +28,7 @@ public class Client {
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
+        // Message display box
         messageBox.setBounds(25, 25, 490, 325);
         messageBox.setFont(new Font("Arial, sans-serif", Font.PLAIN, 15));
         messageBox.setMargin(new Insets(6,6,6,6));
@@ -39,8 +40,42 @@ public class Client {
         messageBoxScroller.setBounds(25, 25, 490, 325);
 
 
+        // Active user box
+        activeUserBox.setBounds(520, 25, 156, 325);
+        activeUserBox.setEditable(false);
+        activeUserBox.setFont(new Font("Arial, sans-serif", Font.PLAIN, 15));
+        activeUserBox.setMargin(new Insets(6, 6, 6, 6));
+        activeUserBox.setContentType("text/html");
+        activeUserBox.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
+
+        JScrollPane userBoxScroller = new JScrollPane(activeUserBox);
+        userBoxScroller.setBounds(520, 25, 156, 325);
+
+        // Message input box
+        promptBox.setBounds(0, 350, 400, 50);
+        promptBox.setFont(new Font("Arial, sans-serif", Font.PLAIN, 15));
+        promptBox.setMargin(new Insets(6, 6, 6, 6));
+
+        JScrollPane inputBoxScroller = new JScrollPane(promptBox);
+        inputBoxScroller.setBounds(25, 350, 650, 50);
+
+        // Send button
+        JButton sendButton = new JButton("Send");
+        sendButton.setFont(new Font("Arial, sans-serif", Font.PLAIN, 15));
+        sendButton.setBounds(575, 410, 100, 35);
+
+
+        // disconnect button
+        JButton disconnectButton = new JButton("Disconnect");
+        disconnectButton.setFont(new Font("Arial, sans-serif", Font.PLAIN, 15));
+        disconnectButton.setBounds(25, 410, 130, 35);
+
+
 
         jFrame.add(messageBox);
+        jFrame.add(activeUserBox);
+        jFrame.add(promptBox);
+        jFrame.add(sendButton);
         jFrame.setVisible(true);
     }
 
@@ -48,3 +83,4 @@ public class Client {
         Client client = new Client();
     }
 }
+
