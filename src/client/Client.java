369e334f6251f033cb209ab7a71cd29596b4227b
Sohan Reza <sohanreja22@gmail.com>
2023-10-16 17:23:43 +0600

    WIP: main window implemented

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,13 @@
 package client;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Client {
+
+    final JTextPane messageBox =  new JTextPane();
+    final JTextPane activeUserBox = new JTextPane();
+    final JTextField promptBox = new JTextField();
 
     private String serverName;
     private int PORT;
@@ -10,5 +17,23 @@ public class Client {
         this.serverName = "localhost";
         this.PORT = 5050;
         this.name = "nickname";
+
+        Font font = new Font("Arial, sans-serif", Font.PLAIN, 15);
+
+        final JFrame jFrame = new JFrame();
+        jFrame.setTitle("Wii");
+        jFrame.setLayout(null);
+        jFrame.setSize(700, 500);
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
     }
 }
